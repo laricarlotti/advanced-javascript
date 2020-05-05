e3c386cc7b37af2eb6a917bdb9b65aac9f716ec0
LariCarlotti <larissa.carlotti@gmail.com>
2020-05-05 11:48:37 -0300

    Comment results on calculateES6.js

--- calculateES6.js
+++ calculateES6.js
@@ -2,8 +2,8 @@ var calculate = function(fn, x, y) {
     return fn(x, y);
 }
 
-const sum = (x, y) => x + y;
+const sum = (x, y) => x + y; 
 const mult = (x, y) => x * y;
 
-calculate(sum, 2, 5); 
-calculate(mult, 3, 4); 
+calculate(sum, 2, 5); // 7
+calculate(mult, 3, 4); // 12
