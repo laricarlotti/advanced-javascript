66067eae8e54bbd639858d8672563de222f13303
LariCarlotti <larissa.carlotti@gmail.com>
2020-05-05 12:17:44 -0300

    Fix SyntaxError in Numbers

--- filterES6.js
+++ filterES6.js
@@ -1,6 +1,6 @@
 // Exercise 01
 
-const numbers = [1, 4, 7, 10];
+const order = [1, 4, 7, 10];
 const isBiggerThanFour = value => value > 4;
 
-const numbersBiggerThanFour = numbers.filter(isBiggerThanFour); // [7, 10]
+const numbersBiggerThanFour = order.filter(isBiggerThanFour); // [7, 10]
