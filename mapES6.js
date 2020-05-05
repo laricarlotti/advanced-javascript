85ad845c5a9d4eed36f875f9c0c3630b361696d4
LariCarlotti <larissa.carlotti@gmail.com>
2020-05-05 12:05:25 -0300

    Finish exercise 02 on mapES6

--- mapES6.js
+++ mapES6.js
@@ -1,4 +1,27 @@
+// Exercise 01
+
 const numbers = [1, 2, 3];
 const square = x =>  x * x;
 const squaredNumbers = numbers.map(square); // [1, 4, 9]
+
+// Exercise 02
+
+const students = [
+    { name: 'Ana', grade: 6 },
+    { name: 'John', grade: 5 },
+    { name: 'Noah', grade: 4 }
+];
+
+const teachers = [
+    { name: 'Mark', salary: 1800 },
+    { name: 'Todd', salary: 2500 },
+    { name: 'Emma', salary: 3200 }
+
+];
+
+const byName = object => object.name;
+const byNames = list => list.map(byName);
+
+byNames(students);
+byNames(teachers);
 
